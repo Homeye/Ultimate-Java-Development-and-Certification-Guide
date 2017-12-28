52561b2936ccc13c83df4e9a848735f936d5ba2e
Máté Jónás <kornjoni@gmail.com>
2017-12-28 16:58:25 +0100

    Section 2 project

---
+++ src/main/java/section2/CapitalizeFirstLetter.java
@@ -0,0 +1,14 @@
+package section2;
+
+public class CapitalizeFirstLetter {
+
+    public static void main(String[] args) {
+
+        String lowercaseString = "this is a very long lower case string";
+
+        if (lowercaseString.length() > 0) {
+            lowercaseString = Character.toUpperCase(lowercaseString.charAt(0)) + lowercaseString.substring(1, lowercaseString.length());
+        }
+        System.out.println(lowercaseString);
+    }
+}
